92618afde35fbe694a5e2da079a70140a5fdf660
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-02-01 02:49:20 +0000

    first step works

--- app.rb
+++ app.rb
@@ -11,12 +11,27 @@ currency_list_raw = HTTP.get(currency_list_url)
 currency_list_parsed = JSON.parse(currency_list_raw)
 currencies_hash = currency_list_parsed.fetch("currencies")
 
+currency_list = []
+currencies_hash.each do |currency, _|
+  currency_list.append(currency)
+end
+
 
 get("/") do
-  @currency_list = []
-  currencies_hash.each do |currency, location|
-    @currency_list.append(currency)
-  end
+  @currency_list = currency_list
   
   erb(:home)
+end
+
+get("/:currency1") do
+  @currency1 = :currency1
+  @currency_list = currency_list
+
+  erb(:step_one)
+end
+
+get("/:currency2") do
+  @currency_list = currency_list
+
+  erb(:step_two)
 end
