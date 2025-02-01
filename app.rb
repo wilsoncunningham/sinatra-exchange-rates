8b35adb75e89cc9e076f3b3bafe66381875de26c
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-02-01 02:31:20 +0000

    getting started with homepage

--- app.rb
+++ app.rb
@@ -1,9 +1,28 @@
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv/load"
+require "json"
+require "http"
+
+EXCHANGE_API_KEY = ENV.fetch("EXCHANGE_API_KEY")
+
+currency_list_url = "https://api.exchangerate.host/list?access_key=#{EXCHANGE_API_KEY}"
+currency_list_raw = HTTP.get(currency_list_url)
+currency_list_parsed = JSON.parse(currency_list_raw)
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  # url = currency_list_url
+  currency_list_raw = HTTP.get(currency_list_url)
+  @currency_list_parsed = JSON.parse(currency_list_raw)
+  @key = EXCHANGE_API_KEY
+
+  @currencies = @currency_list_parsed.fetch("currencies")
+
+  @currency_list = []
+  @currencies.each do |currency, location|
+    @currency_list.append(currency)
+  end
+  
+  erb(:home)
 end
