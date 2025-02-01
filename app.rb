a43a3437a9147e312dca99799b6fb6781db2d957
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-02-01 05:29:20 +0000

    final iteration

--- app.rb
+++ app.rb
@@ -6,25 +6,31 @@ require "http"
 
 EXCHANGE_API_KEY = ENV.fetch("EXCHANGE_API_KEY")
 
-currency_list_url = "https://api.exchangerate.host/list?access_key=#{EXCHANGE_API_KEY}"
-currency_list_raw = HTTP.get(currency_list_url)
-currency_list_parsed = JSON.parse(currency_list_raw)
-currencies_hash = currency_list_parsed.fetch("currencies")
-
-currency_list = []
-currencies_hash.keys.each do |currency|
-  currency_list.append(currency)
-end
-
 
 get("/") do
-  @currency_list = currency_list
+  currency_list_url = "https://api.exchangerate.host/list?access_key=#{EXCHANGE_API_KEY}"
+  currency_list_raw = HTTP.get(currency_list_url)
+  currency_list_parsed = JSON.parse(currency_list_raw)
+  currencies_hash = currency_list_parsed.fetch("currencies")
+
+  @currency_list = []
+  currencies_hash.keys.each do |currency|
+    @currency_list.append(currency)
+  end
   
   erb(:home)
 end
 
 get("/:currency1") do
-  @currency_list = currency_list
+  currency_list_url = "https://api.exchangerate.host/list?access_key=#{EXCHANGE_API_KEY}"
+  currency_list_raw = HTTP.get(currency_list_url)
+  currency_list_parsed = JSON.parse(currency_list_raw)
+  currencies_hash = currency_list_parsed.fetch("currencies")
+
+  @currency_list = []
+  currencies_hash.keys.each do |currency|
+    @currency_list.append(currency)
+  end
 
   erb(:step_one)
 end
